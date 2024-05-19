f1b9771a0ce434bf92cce3b92a8410e9cb6557bd
Mohamed Ashraf <medooka203@gmail.com>
2024-05-19 09:38:17 +0000

    Added HAL/CMSIS as the BSW

---
+++ software/bsw/mcal/hal/tools/cmake/hal_drivers_compile.cmake
@@ -0,0 +1,37 @@
+set(
+    hal_drivers_sources_list
+
+    ${hal_drivers}/src/stm32f1xx_hal_cortex.c
+    ${hal_drivers}/src/stm32f1xx_hal_crc.c
+    ${hal_drivers}/src/stm32f1xx_hal_dma.c
+    ${hal_drivers}/src/stm32f1xx_hal_exti.c
+    ${hal_drivers}/src/stm32f1xx_hal_flash_ex.c
+    ${hal_drivers}/src/stm32f1xx_hal_flash.c
+    ${hal_drivers}/src/stm32f1xx_hal_gpio_ex.c
+    ${hal_drivers}/src/stm32f1xx_hal_gpio.c
+    ${hal_drivers}/src/stm32f1xx_hal_pwr.c
+    ${hal_drivers}/src/stm32f1xx_hal_rcc_ex.c
+    ${hal_drivers}/src/stm32f1xx_hal_rcc.c
+    ${hal_drivers}/src/stm32f1xx_hal_rtc_ex.c
+    ${hal_drivers}/src/stm32f1xx_hal_rtc.c
+    ${hal_drivers}/src/stm32f1xx_hal_tim_ex.c
+    ${hal_drivers}/src/stm32f1xx_hal_tim.c
+    ${hal_drivers}/src/stm32f1xx_hal_uart.c
+    ${hal_drivers}/src/stm32f1xx_hal.c
+)
+
+set(
+    hal_drivers_includes
+
+    ${hal_drivers}/include
+    ${common_platform_includes}
+    ${common_asset_includes}
+)
+
+set(
+    hal_drivers_compile_options
+)
+
+set(
+    hal_drivers_defines
+)
