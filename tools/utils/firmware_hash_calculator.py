c3f9d8b025a8e4b7c8d7a4280c9c7baf1121b301
Mohamed Ashraf <medooka203@gmail.com>
2024-05-19 16:23:07 +0000

    Fixed firmware hash calculator util

--- tools/utils/firmware_hash_calculator.py
+++ tools/utils/firmware_hash_calculator.py
@@ -15,99 +15,62 @@ logging.basicConfig(
 
 class prj_foem_firmware:
     def __init__(self, firmware_fp):
-        self.__firmware_fp = firmware_fp
-        self.__firmware_hex_fp = None
-        self.__firmware_in_bin = None
+        self.__firmware_hex_fp = firmware_fp
+        self.__firmware_bin_fp = self.__firmware_hex_fp
         self.__firmware_size = None
-        self.__hmac_secret_key = b"mohamedAshraf"
-        self.__cmac_secret_key = b"mohamedAshraf"
-        self.__cmac_salt = b"mohamedashraf"
-        self.__ecc_salt = b"mohamedashraf"
 
         # self.__firmware_hex_fetch()
         # self.__firmware_cvt2hex()
-        self.__firmware_cvt2bin()
+        # self.__firmware_cvt2bin()
 
     def __firmware_hex_fetch(self):
         try:
-            with open(self.__firmware_fp, "r") as hex_file:
+            with open(self.__firmware_hex_fp, "r") as hex_file:
                 self.__firmware_hex.append(hex_file.readlines())
             logging.info(f"Fetched the hex file successfully.")
         except FileNotFoundError:
-            logging.error(f"Error: File '{self.__firmware_fp}' not found.")
+            logging.error(f"Error: File '{self.__firmware_hex_fp}' not found.")
 
     def __firmware_cvt2hex(self):
-        ih = IntelHex(self.__firmware_fp)
+        ih = IntelHex(self.__firmware_hex_fp)
         
         ih.tofile("UpdatedFirmware.hex", format='hex')
         
         self.__firmware_hex_fp = "./UpdatedFirmware.hex"
 
     def __firmware_cvt2bin(self):
-        ih = IntelHex(self.__firmware_fp)
+        ih = IntelHex(self.__firmware_hex_fp)
 
         ih.tobinfile("UpdatedFirmware.bin")
-        self.__firmware_fp_bin = "./UpdatedFirmware.bin"
+        self.__firmware_hex_fp_bin = "./UpdatedFirmware.bin"
     
     def __calculate_sha256(self):
         sha256_hash = hashlib.sha256()
-        with open(self.__firmware_fp_bin, "rb") as f:
+        with open(self.__firmware_bin_fp, "rb") as f:
             # Read the file in chunks of 4096 bytes
             for byte_block in iter(lambda: f.read(4096), b""):
                 sha256_hash.update(byte_block)
         return str(sha256_hash.hexdigest())
 
-    def __calculate_cmac(self):
-        sha256_hash = self.__calculate_sha256()
-        cmac_key = PBKDF2(self.__cmac_secret_key, self.__cmac_salt, dkLen=16)
-        cmac_cipher = CMAC.new(cmac_key, ciphermod=AES)
-        cmac_cipher.update(sha256_hash)
-        cmac_digest = cmac_cipher.hexdigest()
-        return cmac_digest
-
-    def __calculate_hmac(self):
-        hmac_obj = HMAC.new(str(self.__hmac_secret_key).encode(
-            'utf-8'), digestmod=SHA256)
-        hmac_obj.update(str(self.__calculate_sha256()).encode('utf-8'))
-        return hmac_obj.digest()
-
     def __firmware_calculate_size(self):
         try:
-            with open(self.__firmware_fp, "r") as hex_file:
+            with open(self.__firmware_bin_fp, "rb") as bin_fp:
                 firmware_size = 0
-                for line in hex_file:
-                    # Remove newline characters and any leading/trailing whitespaces
-                    hex_line = line.strip()
-                    # Each line has a record type in the last two characters, which we exclude from size calculation
-                    if hex_line and hex_line[-2:] != "00":
-                        # Each byte is represented by two hex digits, so increment size by half the hex length
-                        firmware_size += len(hex_line) // 2
-
+                
+                # Iterate through the binary file byte by byte
+                byte = bin_fp.read(1)
+                while byte:
+                    firmware_size += 1
+                    byte = bin_fp.read(1)
+                
+                self.__firmware_size = firmware_size
                 return firmware_size
-            logging.info(f"Calculated firmware size successfully.")
         except FileNotFoundError:
-            logging.error(f"Error: File '{self.__firmware_fp}' not found.")
+            logging.error(f"Error: File '{self.__firmware_bin_fp}' not found.")
             return None
-
-    def __firmware_encrypt_ecc(self):
-        pass
-
-    def __firmware_dsa(self):
-        pass
-    #
-    #
 
     def get_sha256(self):
         return self.__calculate_sha256()
-
-    def get_cmac(self):
-        return self.__calculate_cmac()
-
-    def get_hmac(self):
-        return self.__calculate_hmac()
-
-    def enc_firmware(self):
-        return self.__firmware_encrypt_ecc()
 
     def get_size(self):
         return self.__firmware_calculate_size()
@@ -120,5 +83,5 @@ if __name__ == '__main__':
     file_path = args.file_path
 
     myFirmware = prj_foem_firmware(file_path)
-    print(f'size: {myFirmware.get_size()}')
-    print(f'SHA256-Hash: {myFirmware.get_sha256()}')
+    # print(f'{myFirmware.get_size()}')
+    print(f'{myFirmware.get_sha256()}')
