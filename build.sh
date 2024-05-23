5e5593f54b3f86a3a4478e6d7c006861b13e43f1
Mohamed Ashraf <medooka203@gmail.com>
2024-05-23 16:28:51 +0000

    Added script rebuild option

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+#
+rebuild=$1
+#
 BTL_SWC_PATH_BIN="/workspace/tools/build/CMake/output/build/cf4a_gp_bootloader.bin"
 TA_SWC_PATH_BIN="/workspace/tools/build/CMake/output/build/cf4a_gp_targetapp.bin"
 BTL_SWC_PATH_HEX="/workspace/tools/build/CMake/output/build/cf4a_gp_bootloader.hex"
 TA_SWC_PATH_HEX="/workspace/tools/build/CMake/output/build/cf4a_gp_targetapp.hex"
 TA_SWC_RDR_PATH="/workspace/website/fota/firmwares"
-
 # Color codes for styling
 RED="\033[0;31m"
 GREEN="\033[0;32m"
@@ -13,21 +14,18 @@ YELLOW="\033[0;33m"
 BLUE="\033[0;34m"
 BOLD="\033[1m"
 RESET="\033[0m"
-
 # Function to echo colored text
 echo_color() {
     local color=$1
     local message=$2
     echo -e "${color}${message}${RESET}"
 }
-
 # Function to check if a file exists
 check_file_exists() {
     if [ ! -f "$1" ]; then
         echo_color $RED "${BOLD}Error: File '$1' not found.${RESET}"
     fi
 }
-
 # Pre conditions
 echo ------------------------------------------------------------------------------------------------------
 echo_color $GREEN "${BOLD}Install Depends [utils]${RESET}"
@@ -35,14 +33,22 @@ if ! pip install -r tools/utils/requirements.txt; then
     echo_color $RED "${BOLD}Error: Failed to install dependencies.${RESET}"
 fi
 echo ------------------------------------------------------------------------------------------------------
-
 # Build the project
 echo_color $BLUE "${BOLD}Building Software${RESET}"
-if ! make build_bootloader || ! make build_target_app; then
-    echo_color $RED "${BOLD}Error: Build failed.${RESET}"
+
+if [ "$rebuild" == "1" ]; then
+    echo_color $BLUE "${BOLD}Rebuilding is True${RESET}"
+    if ! make build_bootloader rebuild=1 || ! make build_target_app rebuild=1; then
+        echo_color $RED "${BOLD}Error: Build failed.${RESET}"
+        exit 1
+    fi
+else
+    if ! make build_bootloader || ! make build_target_app; then
+        echo_color $RED "${BOLD}Error: Build failed.${RESET}"
+        exit 1
+    fi
 fi
 echo ------------------------------------------------------------------------------------------------------
-
 # Run hash calculator on output [Bootloader SWC]
 echo_color $YELLOW "${BOLD}Calculating cf4a_gp_bootloader hash [SHA256] and size [BYTES].${RESET}"
 check_file_exists "${BTL_SWC_PATH_HEX}"
