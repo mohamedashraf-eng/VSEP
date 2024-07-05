085a459b4c8e3db0a220bf38a846d5305a8029cd
Mohamed Ashraf <medooka203@gmail.com>
2024-07-05 22:43:50 +0000

    Fix firmware file duplication bug

--- tools/utils/Website/ws_db_insert.sh
+++ tools/utils/Website/ws_db_insert.sh
@@ -11,15 +11,19 @@ TABLE_NAME="website_fota_firmware"
 VERSION=$1
 RELEASE_DATE="2024-06-29"
 OLD_FIRMWARE_FILE="~/Documents/CMAKE_GP/website/fota/firmwares/UpdatedFirmware.hex"
-FIRMWARE_FILE="~/Documents/GP/Grad_Project/src/oem_intf/django_webappv1/fota/firmwares/UpdatedFirmware.hex"
+FIRMWARE_DIR="~/Documents/GP/Grad_Project/src/oem_intf/django_webappv1/fota/firmwares"
 HASH_FILE_PATH="~/Documents/CMAKE_GP/website/fota/firmwares/UpdatedFirmwareHash.txt"
 IS_ACTIVE=1
 DESCRIPTION=""
 
 # Expand paths
 OLD_FIRMWARE_FILE=$(eval echo $OLD_FIRMWARE_FILE)
-FIRMWARE_FILE=$(eval echo $FIRMWARE_FILE)
+FIRMWARE_DIR=$(eval echo $FIRMWARE_DIR)
 HASH_FILE_PATH=$(eval echo $HASH_FILE_PATH)
+
+# Generate a random hash for the new firmware file name
+RANDOM_HASH=$(openssl rand -hex 16)
+NEW_FIRMWARE_FILE="${FIRMWARE_DIR}/UpdatedFirmware_${RANDOM_HASH}.hex"
 
 # Extract hash and size from the hash file
 extract_hash_and_size() {
@@ -37,11 +41,11 @@ extract_hash_and_size() {
 
 read firmware_hash firmware_size <<< $(extract_hash_and_size "$HASH_FILE_PATH")
 
-# Copy the old firmware file to the new location
-cp "$OLD_FIRMWARE_FILE" "$FIRMWARE_FILE"
+# Copy the old firmware file to the new location with the new filename
+cp "$OLD_FIRMWARE_FILE" "$NEW_FIRMWARE_FILE"
 
 # MySQL command to insert a new record
-INSERT_QUERY="INSERT INTO $TABLE_NAME (version, release_date, firmware, is_active, firmware_cmac, firmware_size, description) VALUES ('$VERSION', '$RELEASE_DATE', '$FIRMWARE_FILE', $IS_ACTIVE, '$firmware_hash', '$firmware_size', '$DESCRIPTION');"
+INSERT_QUERY="INSERT INTO $TABLE_NAME (version, release_date, firmware, is_active, firmware_cmac, firmware_size, description) VALUES ('$VERSION', '$RELEASE_DATE', '$NEW_FIRMWARE_FILE', $IS_ACTIVE, '$firmware_hash', '$firmware_size', '$DESCRIPTION');"
 
 # Execute the MySQL command
 mysql -u"$DB_USER" -h "$DB_HOST" -D "$DB_NAME" -e "$INSERT_QUERY"
